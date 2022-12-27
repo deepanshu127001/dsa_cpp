51f33cd46c043c073e2ae2b2f5d03e79fa706dc9
deepanshu127001 <deepanshu127001@gmail.com>
2022-12-28 02:43:31 +0530

    circular linked list

--- 6 circular_linked_list/main.cpp
+++ 6 circular_linked_list/main.cpp
@@ -1,22 +1,67 @@
+#include <iostream>
 struct Node {
-    int data;
-    Node *next;
+  int data;
+  Node *next;
 };
-class CLL{
-    Node *last;
-    public:
-    CLL();
-    void insertAtBeg(int);
+class CircularList{
+  Node *last;
+  public:
+  CircularList();
+  void insertAtBeg(int);
+  void insertAtLast(int);
+  void traverse();
 };
-CLL::CLL(){
-    last = nullptr;
+CircularList::CircularList(){
+  last = nullptr;
 }
-void CLL::insertAtBeg(int data){
-    Node *node=new Node;
-    if(last){
-
+void CircularList::insertAtBeg(int data){
+  Node *new_node =  new Node;
+  new_node -> data = data;
+  if(last){
+    new_node->next = last->next;
+    last->next = new_node;
+    
+  }
+  else{
+    new_node->next = new_node;
+    last = new_node;
+  }
+}
+void CircularList::insertAtLast(int data){
+  Node *new_node =  new Node;
+  new_node->data = data;
+  if(last){
+    new_node -> next = last -> next;
+    last->next = new_node;
+    last = new_node;
+  }
+  else{
+    new_node -> next = new_node;
+    last = new_node;
+    
+  }
+}
+void CircularList::traverse(){
+  Node *start;
+  if(last){
+    start=last->next;
+    while(start!=last){
+      std::cout<<start->data<<" ";
+      start=start->next;
     }
-    else{
-        node->next=node;
-    }
+      std::cout<<start->data<<" ";
+    
+  }
+}
+int main(){
+  CircularList c1;
+  c1.insertAtBeg(3);
+  c1.insertAtBeg(2);
+  c1.insertAtBeg(1);
+  c1.insertAtLast(4);
+  c1.insertAtLast(5);
+  c1.insertAtLast(6);
+  c1.insertAtLast(7);
+  c1.insertAtLast(8);
+  c1.traverse();
 }
