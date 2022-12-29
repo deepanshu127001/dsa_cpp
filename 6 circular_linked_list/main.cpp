c6e20c339fdc2f36457cd3175c0c2bb8195efbc9
deepanshu127001 <deepanshu127001@gmail.com>
2022-12-29 22:05:02 +0530

    added delete end method

--- 6 circular_linked_list/main.cpp
+++ 6 circular_linked_list/main.cpp
@@ -1,86 +1,142 @@
 #include <iostream>
-struct Node {
+struct Node
+{
   int data;
   Node *next;
 };
-class CircularList{
+class CircularList
+{
   Node *last;
-  public:
+
+public:
   CircularList();
   void insertAtBeg(int);
   void insertAtLast(int);
   void traverse();
-  Node* search (int);
-  void insertAfter(Node*,int);
+  Node *search(int);
+  void insertAfter(Node *, int);
+  void deleteFirst();
+  void deleteEnd();
+  void deleteAfter();
 };
-void CircularList::insertAfter(Node *temp,int data){
-     Node *node=new Node;
-     node->data = data;
-     if(last){
-      node->next=temp->next;
-      temp->next=node;
-      if(temp == last )
-        last =node;
-     }
+void CircularList::insertAfter(Node *temp, int data)
+{
+  Node *node = new Node;
+  node->data = data;
+  if (last)
+  {
+    node->next = temp->next;
+    temp->next = node;
+    if (temp == last)
+      last = node;
+  }
 }
-CircularList::CircularList(){
+CircularList::CircularList()
+{
   last = nullptr;
 }
-void CircularList::insertAtBeg(int data){
-  Node *new_node =  new Node;
-  new_node -> data = data;
-  if(last){
+void CircularList::insertAtBeg(int data)
+{
+  Node *new_node = new Node;
+  new_node->data = data;
+  if (last)
+  {
     new_node->next = last->next;
     last->next = new_node;
-    
   }
-  else{
+  else
+  {
     new_node->next = new_node;
     last = new_node;
   }
 }
-void CircularList::insertAtLast(int data){
-  Node *new_node =  new Node;
+void CircularList::insertAtLast(int data)
+{
+  Node *new_node = new Node;
   new_node->data = data;
-  if(last){
-    new_node -> next = last -> next;
+  if (last)
+  {
+    new_node->next = last->next;
     last->next = new_node;
     last = new_node;
   }
-  else{
-    new_node -> next = new_node;
+  else
+  {
+    new_node->next = new_node;
     last = new_node;
-    
   }
 }
-void CircularList::traverse(){
+void CircularList::traverse()
+{
   Node *start;
-  if(last){
-    start=last->next;
-    while(start!=last){
-      std::cout<<start->data<<" ";
-      start=start->next;
+  if (last)
+  {
+    start = last->next;
+    while (start != last)
+    {
+      std::cout << start->data << " ";
+      start = start->next;
     }
-      std::cout<<start->data<<" ";
-    
+    std::cout << start->data << " ";
   }
 }
-Node* CircularList::search(int data){
-  if(last){
-    Node *temp=last;
+Node *CircularList::search(int data)
+{
+  if (last)
+  {
+    Node *temp = last;
     do
     {
-      if(temp->data == data)
+      if (temp->data == data)
       {
         return temp;
       }
-      temp=temp->next;
-    } while(temp!=last);
+      temp = temp->next;
+    } while (temp != last);
   }
   return nullptr;
 }
-
-int main(){
+void CircularList::deleteFirst()
+{
+  if (last)
+  {
+    Node *r = last->next;
+    if (last->next == last)
+    {
+      last = nullptr;
+    }
+    else
+    {
+      last->next = r->next;
+    }
+    delete r;
+  }
+}
+void CircularList::deleteEnd()
+{
+  Node *r = last;
+  if (last)
+  {
+    if (last->next == last)
+    {
+      delete last;
+      last = nullptr;
+    }
+    else
+    {
+      Node *temp = last->next;
+      while (temp->next != last)
+      {
+        temp = temp->next;
+      }
+      temp->next = last->next;
+      last=temp;
+    }
+    delete r;
+  }
+}
+int main()
+{
   CircularList c1;
   c1.insertAtBeg(3);
   c1.insertAtBeg(2);
@@ -90,6 +146,8 @@ int main(){
   c1.insertAtLast(6);
   c1.insertAtLast(7);
   c1.insertAtLast(8);
-  c1.insertAfter(c1.search(8),345);
+  c1.insertAfter(c1.search(8), 345);
+  c1.traverse();
+  c1.deleteEnd();
   c1.traverse();
 }
