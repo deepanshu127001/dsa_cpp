f1c5e142140a2969e3d7be40d3e8aea75226d013
deepanshu127001 <deepanshu127001@gmail.com>
2022-12-30 23:22:16 +0530

    added insertAtBegining

--- 8 circular_doubly_linked_list/main.cpp
+++ 8 circular_doubly_linked_list/main.cpp
@@ -1,42 +1,62 @@
 #include <iostream>
-struct Node{
+struct Node {
     int data;
     Node *prev;
     Node *next;
 };
 class CDLL{
-    Node *last;
-    public:
+    Node *start;
+    public :
     CDLL();
-    void insertAtBeg(int);
+    void insertAtBegining(int);
+    void insertAtEnd(int);
+    Node* search(int);
+    void insertAfter(Node*,int);
+    void deleteFirst();
+    void deleteLast();
+    void deleteNode(Node*);
     void traverse();
+    // ~CDLL();
 };
-CDLL::CDLL(){
-    last = nullptr;
-}
-void CDLL::insertAtBeg(int data){
-    Node *new_node = new Node;
-    new_node->data = data;
-    if(last == nullptr){
-        last = new_node ;
-        new_node->prev=new_node;
-        new_node->next=new_node;
+void CDLL::traverse(){
+    if(start==nullptr){
+        std::cout<<"list is empty ";
     }
     else{
-
+        Node *temp=start;
+        do{
+            std::cout<<temp->data<<" ";
+            temp=temp->next;
+        }while(temp!=start);
     }
 }
-void CDLL::traverse(){
-    if(last){
-        Node *temp=last;
-        do{
-            std::cout<<temp -> data;
-            temp = temp->next;
-        }while(temp!=last);
+CDLL::CDLL(){
+    start = nullptr;
+}
+void CDLL::insertAtBegining(int data){
+    Node *node=new Node;
+    node -> data = data;
+    if(start==nullptr){
+        node->prev=node;
+        node->next=node;
+        start = node;
+    }
+    else{
+        node->next=start;
+        node->prev=start->prev;
+        start->prev->next=node;
+        start->prev=node;
+        start=node;
     }
 }
 int main(int argc, char const *argv[])
 {
-    
+    CDLL l1;
+    l1.insertAtBegining(5);
+    l1.insertAtBegining(4);
+    l1.insertAtBegining(3);
+    l1.insertAtBegining(2);
+    l1.insertAtBegining(1);
+    l1.traverse();
     return 0;
 }
